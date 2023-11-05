7ee046749c58fe9e421266c551b791e2fd91264f
yarivShamash <yariv.shamash@gmail.com>
2023-11-05 16:28:36 +0000

    solution

--- typescript/resistor-color/resistor-color.ts
+++ typescript/resistor-color/resistor-color.ts
@@ -1,5 +1,15 @@
-export const colorCode = () => {
-  throw new Error('Delete this line and implement this function')
-}
+export const COLORS = [
+    'black',
+    'brown',
+    'red',
+    'orange',
+    'yellow',
+    'green',
+    'blue',
+    'violet',
+    'grey',
+    'white',
+]
 
-export const COLORS = undefined
+export const colorCode = (colorName: string): number =>
+    COLORS.indexOf(colorName)
