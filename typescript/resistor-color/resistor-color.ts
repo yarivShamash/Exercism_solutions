66f76858e957bdb20a3b2806d0a49b25933fd858
yarivShamash <yariv.shamash@gmail.com>
2023-11-05 16:39:12 +0000

    Error handling added

--- typescript/resistor-color/resistor-color.ts
+++ typescript/resistor-color/resistor-color.ts
@@ -1,15 +1,23 @@
+const errorMessage = `The color you passed is not in the list of colors.
+        Try black, brown, red, orange, yellow, green, blue, violet, grey or white.`
+
 export const COLORS = [
-    'black',
-    'brown',
-    'red',
-    'orange',
-    'yellow',
-    'green',
-    'blue',
-    'violet',
-    'grey',
-    'white',
+  'black',
+  'brown',
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'blue',
+  'violet',
+  'grey',
+  'white',
 ]
 
-export const colorCode = (colorName: string): number =>
-    COLORS.indexOf(colorName)
+export const colorCode = (colorName: string): number => {
+  const colorIndex = COLORS.indexOf(colorName)
+  if (colorIndex === -1) {
+    throw new Error(errorMessage)
+  }
+  return colorIndex
+}
