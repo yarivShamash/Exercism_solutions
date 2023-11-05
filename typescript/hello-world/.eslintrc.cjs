21e81c5300156017a8feaa362967aa97c47eb907
yarivShamash <yariv.shamash@gmail.com>
2023-11-05 16:26:26 +0000

    resistor-color exercise config and pretty

--- typescript/hello-world/.eslintrc.cjs
+++ typescript/hello-world/.eslintrc.cjs
@@ -1,41 +1,43 @@
 module.exports = {
-  root: true,
-  rules: {
-    semi: "always",
-  },
-  parserOptions: {
-    tsconfigRootDir: __dirname,
-    project: ["./tsconfig.json"],
-  },
-  overrides: [
-    // Student provided files
-    {
-      files: ["*.ts"],
-      excludedFiles: [".meta/proof.ci.ts", ".meta/exemplar.ts", "*.test.ts"],
-      extends: "@exercism/eslint-config-typescript",
+    root: true,
+
+    parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: ['./tsconfig.json'],
     },
-    // Exercism given tests
-    {
-      files: ["*.test.ts"],
-      excludedFiles: ["custom.test.ts"],
-      env: {
-        jest: true,
-      },
-      extends: "@exercism/eslint-config-typescript/maintainers",
-    },
-    // Student provided tests
-    {
-      files: ["custom.test.ts"],
-      env: {
-        jest: true,
-      },
-      extends: "@exercism/eslint-config-typescript",
-    },
-    // Exercism provided files
-    {
-      files: [".meta/proof.ci.ts", ".meta/exemplar.ts", "*.test.ts"],
-      excludedFiles: ["custom.test.ts"],
-      extends: "@exercism/eslint-config-typescript/maintainers",
-    },
-  ],
-};
+    overrides: [
+        // Student provided files
+        {
+            files: ['*.ts'],
+            excludedFiles: [
+                '.meta/proof.ci.ts',
+                '.meta/exemplar.ts',
+                '*.test.ts',
+            ],
+            extends: '@exercism/eslint-config-typescript',
+        },
+        // Exercism given tests
+        {
+            files: ['*.test.ts'],
+            excludedFiles: ['custom.test.ts'],
+            env: {
+                jest: true,
+            },
+            extends: '@exercism/eslint-config-typescript/maintainers',
+        },
+        // Student provided tests
+        {
+            files: ['custom.test.ts'],
+            env: {
+                jest: true,
+            },
+            extends: '@exercism/eslint-config-typescript',
+        },
+        // Exercism provided files
+        {
+            files: ['.meta/proof.ci.ts', '.meta/exemplar.ts', '*.test.ts'],
+            excludedFiles: ['custom.test.ts'],
+            extends: '@exercism/eslint-config-typescript/maintainers',
+        },
+    ],
+}
