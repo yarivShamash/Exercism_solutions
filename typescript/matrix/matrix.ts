3b797b4e9943e1816aee2d9cfc9cc0b92e0df970
yarivShamash <yariv.shamash@gmail.com>
2023-11-07 19:29:11 +0000

    Solution

--- typescript/matrix/matrix.ts
+++ typescript/matrix/matrix.ts
@@ -1,13 +1,32 @@
 export class Matrix {
-  constructor() {
-    throw new Error('Remove this statement and implement this function')
+  private matrix: number[][] = []
+  private matrixByColumn: number[][] = []
+
+  constructor(matrix: string) {
+    const matrixRows = matrix.split('\n')
+    this.matrix = matrixRows.map((matrixRow) => {
+      return matrixRow.split(' ').map((value) => Number(value))
+    })
   }
 
-  get rows(): unknown {
-    throw new Error('Remove this statement and implement this function')
+  get rows(): number[][] {
+    return this.matrix
   }
 
-  get columns(): unknown {
-    throw new Error('Remove this statement and implement this function')
+  get columns(): number[][] {
+    this.matrix.forEach(
+      (row) => {
+        row.forEach((number, index) => {
+          if (!this.matrixByColumn[index]) {
+            this.matrixByColumn[index] = []
+          }
+          this.matrixByColumn[index].push(number)
+        })
+        return this.matrixByColumn
+      },
+      Array.from({ length: this.matrix[0].length }, (): number[] => [])
+    )
+
+    return this.matrixByColumn
   }
 }
