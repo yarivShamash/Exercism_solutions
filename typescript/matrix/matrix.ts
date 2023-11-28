f51e7fdf13c578513e7ff43479a45523e1d5d306
yarivShamash <yariv.shamash@gmail.com>
2023-11-28 12:53:14 +0000

    get columns() refactored

--- typescript/matrix/matrix.ts
+++ typescript/matrix/matrix.ts
@@ -1,12 +1,11 @@
 export class Matrix {
   private matrix: number[][] = []
-  private matrixByColumn: number[][] = []
 
   constructor(matrix: string) {
     const matrixRows = matrix.split('\n')
-    this.matrix = matrixRows.map((matrixRow) => {
-      return matrixRow.split(' ').map((value) => Number(value))
-    })
+    this.matrix = matrixRows.map((matrixRow) =>
+      matrixRow.split(' ').map((value) => Number(value))
+    )
   }
 
   get rows(): number[][] {
@@ -14,19 +13,8 @@ export class Matrix {
   }
 
   get columns(): number[][] {
-    this.matrix.forEach(
-      (row) => {
-        row.forEach((number, index) => {
-          if (!this.matrixByColumn[index]) {
-            this.matrixByColumn[index] = []
-          }
-          this.matrixByColumn[index].push(number)
-        })
-        return this.matrixByColumn
-      },
-      Array.from({ length: this.matrix[0].length }, (): number[] => [])
+    return this.matrix[0].map((_, columnIndex) =>
+      this.matrix.map((row) => row[columnIndex])
     )
-
-    return this.matrixByColumn
   }
 }
